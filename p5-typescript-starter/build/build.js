2a5227be2035adcd977d3440474a7519503efba6
Costantini Milos, SUB-BLO <Milos.Costantini@swisscom.com>
2020-03-07 18:05:26 +0100

    non simmetric star

--- p5-typescript-starter/build/build.js
+++ p5-typescript-starter/build/build.js
@@ -84,6 +84,7 @@ var angle = 0;
 var squares = 10;
 var colors;
 function setup() {
+    frameRate(35);
     createCanvas(windowWidth, windowHeight);
     rectMode(CENTER);
     colors = ColorHelper.getColorsArray(squares);
@@ -94,16 +95,7 @@ function draw() {
     angle = angle + 0.01;
     rotate(angle / 2);
     for (var i = 0; i < squares; i++) {
-        Shapes.drawNestedStar(10, colors, i, i);
-    }
-    translate(300, 400);
-    angle = angle + 12.05;
-    for (var i = 0; i < squares; i++) {
-        Shapes.drawNestedStar(10, colors, i * 2, i * 2);
-    }
-    for (var i = 0; i < squares; i++) {
-        translate(300, 400);
-        Shapes.drawNestedStar(10, colors, i / 2, i / 2);
+        Shapes.drawNestedStar(30, colors, i, i);
     }
 }
 //# sourceMappingURL=build.js.map
--- p5-typescript-starter/sketch/sketch.ts
+++ p5-typescript-starter/sketch/sketch.ts
@@ -3,6 +3,7 @@ let squares = 10;
 let colors: p5.Color[];
 
 function setup() {
+  frameRate(35);
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   colors = ColorHelper.getColorsArray(squares);
@@ -16,18 +17,6 @@ function draw() {
   rotate(angle / 2);
 
   for (var i = 0; i < squares; i++) {
-    Shapes.drawNestedStar(10, colors, i, i);
-  }
-
-  translate(300, 400);
-  angle = angle + 12.05;
-  for (var i = 0; i < squares; i++) {
-    Shapes.drawNestedStar(10, colors, i * 2, i * 2);
-  }
-
-  for (var i = 0; i < squares; i++) {
-    translate(300, 400);
-
-    Shapes.drawNestedStar(10, colors, i / 2, i / 2);
+    Shapes.drawNestedStar(30, colors, i, i);
   }
 }
